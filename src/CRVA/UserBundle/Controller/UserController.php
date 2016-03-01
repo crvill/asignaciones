1889df24c3b4d63904a2a3c3a6219ae30f881a85
crvill <crvillalvial@gmail.com>
2016-03-01 14:59:25 -0300

    video 13 y 14

--- src/CRVA/UserBundle/Controller/UserController.php
+++ src/CRVA/UserBundle/Controller/UserController.php
@@ -5,6 +5,8 @@ namespace CRVA\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\FormError;
 use CRVA\UserBundle\Entity\User;
 use CRVA\UserBundle\Form\UserType;
 
@@ -25,16 +27,19 @@ class UserController extends Controller
 
         return new Response($res);**/
 
-        $dql = "SELECT u FROM CRVAUserBundle:User u";
+        $dql = "SELECT u FROM CRVAUserBundle:User u ORDER BY u.id DESC";
         $users = $em->createQuery($dql);
 
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
             $users,$request->query->getInt('page',1),
-            6
+            8
         );
 
-        return $this->render('CRVAUserBundle:User:index.html.twig', array('pagination' => $pagination));
+        $deleteFormAjax = $this->createCustomForm(':USER_ID', 'DELETE', 'crva_user_delete');
+
+        return $this->render('CRVAUserBundle:User:index.html.twig', array('pagination' => $pagination,
+            'delete_form_ajax' => $deleteFormAjax->createView()));
     }
 
     public function addAction()
@@ -64,40 +69,200 @@ class UserController extends Controller
 
             $password = $form->get('password')->getData();
 
-            $encoder = $this->container->get('security.password_encoder');
-            $encoder = $encoder->encodePassword($user,$password);
-            
-            $user->setPassword($encoder);
+            $passwordConstraint = new Assert\NotBlank();
+            $errorList = $this->get('validator')->validate($password,$passwordConstraint);
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+            if(count($errorList) == 0){
+                $encoder = $this->container->get('security.password_encoder');
+                $encoder = $encoder->encodePassword($user,$password);
+                
+                $user->setPassword($encoder);
 
-            $successMessage = $this->get('translator')->trans('The user has been created.');
-            $this->addFlash('mensaje', $successMessage);
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
 
-            return $this->redirectToRoute("crva_user_index"); 
+                $successMessage = $this->get('translator')->trans('The user has been created.');
+                $this->addFlash('mensaje', $successMessage);
+
+                return $this->redirectToRoute("crva_user_index");
+            }else{
+                $errorMessage = new FormError($errorList[0]->getMessage());
+                $form->get('password')->addError('errorMessage');
+            }
         }
 
         return $this->render('CRVAUserBundle:User:add.html.twig', array('form' => $form->createView()));
     }
 
-    public function editAction()
+    public function editAction($id)
     {
-        
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('CRVAUserBundle:User')->find($id);
+
+        if(!$user){
+            $messageException = $this->get('translator')->trans('User not found');
+            throw $this->createNotFoundException($messageException);
+        }
+
+        $form = $this->createEditForm($user);
+
+        return $this->render('CRVAUserBundle:User:edit.html.twig', array('user'=>$user, 'form'=>$form->createView()));
+    }
+
+    private function createEditForm(User $entity)
+    {
+        $form = $this->createForm(new UserType(), $entity, array('action' => $this->generateUrl
+            ('crva_user_update', array('id' => $entity->getId())), 'method'=>'PUT'));
+
+        return $form;
+    }
+
+    public function updateAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('CRVAUserBundle:User')->find($id);
+
+        if(!$user){
+            $messageException = $this->get('translator')->trans('User not found');
+            throw $this->createNotFoundException($messageException);
+        }
+
+        $form = $this->createEditForm($user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() & $form->isValid()){
+            $password = $form->get('password')->getData();
+
+            if(!empty($password)){
+                $encoder = $this->container->get('security.password_encoder');
+                $encoder = $encoder->encodePassword($user,$password);
+                $user->setPassword($encoder);
+
+            }else{
+                $recoverPass = $this->recoverPass($id);
+                #print_r($recoverPass);
+                #exit();
+                $user->setPassword($recoverPass[0]['password']);
+            }
+
+            if($form->get('role')->getData() == 'ROLE_ADMIN'){
+                $user->setIsActive(1);
+            }
+
+            $em->flush();
+            $successMessage = $this->get('translator')->trans('The user has been modified.');
+            $this->addFlash('mensaje',$successMessage);
+
+            return $this->redirectToRoute('crva_user_edit', array('id'=> $user->getId()));
+        }
+
+        return $this->render('CRVAUserBundle:User:edit.html.twig', array('user'=>$user, 'form'=>$form->createView()));
+    }
+
+    private function recoverPass($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery(
+            'SELECT u.password
+            FROM CRVAUserBundle:User u
+            WHERE u.id = :id'
+            )->setParameter('id', $id);
+
+        $currentPass = $query->getResult();
+
+        return $currentPass;   
     }
 
     public function viewAction($id)
     {
-        $em = $this->getDoctrine()->getRepository('CRVAUserBundle:User');
+        #$em = $this->getDoctrine()->getRepository('CRVAUserBundle:User');
 
-        $user = $em->find($id);
+        #$user = $em->find($id);
 
-        return new Response('Usuario: ' . $user->getUsername() . ' con Email: '. $user->getEmail(). '<br/>');    
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('CRVAUserBundle:User')->find($id);
+
+        if(!$user){
+            $messageException = $this->get('translator')->trans('User not found');
+            throw $this->createNotFoundException($messageException);
+        } 
+
+        $deleteForm = $this->createCustomForm($user->getId(), 'DELETE', 'crva_user_delete' );
+
+        return $this->render('CRVAUserBundle:User:view.html.twig', array('user' => $user, 'delete_form' => $deleteForm->createView()));
+
     }
 
-    public function deleteAction()
+    //public function createDeleteForm($user)
+    //{
+    //    return $this->createFormBuilder()
+    //        ->setAction($this->generateUrl('crva_user_delete', array('id' => $user->getId())))
+    //        ->setMethod('DELETE')
+    //        ->getForm();
+    //    
+    //}
+    public function deleteAction(Request $request, $id)
     {
-        
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('CRVAUserBundle:User')->find($id);
+
+        if(!$user){
+            $messageException = $this->get('translator')->trans('User not found');
+            throw $this->createNotFoundException($messageException);
+        } 
+
+        //$form = $this->createDeleteFOrm($user);
+        $allUsers = $em->getRepository('CRVAUserBundle:User')->findAll();
+        $countUsers = count($allUsers);
+
+        $form = $this->createCustomForm($user->getId(), 'DELETE', 'crva_user_delete');
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            if($request->isXMLHttpRequest()){
+                $res = $this->deleteUser($user->getRole(), $em, $user);
+
+                return new Response(
+                    json_encode(array('removed'=>$res['removed'], 'message' => $res['message'], 'countUsers' => $countUsers)), 
+                    200, 
+                    array('Content-Type' => 'application')
+                );
+            }
+
+            $res = $this->deleteUser($user->getRole(), $em,$user);
+
+            $this->addFlash($res['alert'], $res['message']);
+
+            return $this->redirectToRoute('crva_user_index');
+        }
+    }
+
+    private function deleteUser($role, $em, $user)
+    {
+        if($role == 'ROLE_USER'){
+            $em->remove($user);
+            $em->flush();
+
+            $message = $this->get('translator')->trans('The user has been deleted.');
+            $remove = 1;
+            $alert = 'mensaje';
+        }elseif($role == 'ROLE_ADMIN'){
+            $message = $this->get('translator')->trans('The user cold not be deleted.');
+            $remove = 0;
+            $alert = 'error';
+        }
+
+        return array('removed' => $remove, 'message'=> $message, 'alert' => $alert);
+    }
+
+    private function createCustomForm($id,$method,$route)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl($route, array('id' => $id)))
+            ->setMethod($method)
+            ->getForm();
     }
 }
